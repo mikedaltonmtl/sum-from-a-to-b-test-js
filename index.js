7aa9c91f920195d4e3c10dcca92a90cddc39510f
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-10-08 14:59:10 +0000

    cleaned comments

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 /* Make sum a recursive function that sums all the whole numbers from fromN to toN
  * For this exercise, we can safely assume that fromN will always be less than or equal to toN
  */
+
 const sum = function(fromN, toN) {
   // base case: fromN = toN
   if (fromN === toN) {
     return fromN;
   }
 
-  console.log('toN', toN);
-
-  // call function recursively with the increased value of fromN
+  // call function recursively with the value of toN decreased
   return toN + sum(fromN, toN - 1);
 
-  // how is 'return' storing the accumulated value of each iteration???
 };
 
 console.log(sum(3, 7));
